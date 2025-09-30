485d3359e4bb26f5ea0a818166917d6a2804a2d5
More-F <andresfel38@gmail.com>
2025-09-30 17:45:00 -0500

    Agregacion de estilos para paginas

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ['pregunta', 'orden']  # Columnas que se ven en la lista
+    list_editable = ['orden']  # Puedes editar el orden directamente
+    search_fields = ['pregunta', 'respuesta']  # Buscar en estos campos
+    list_filter = ['orden']  # Filtros en la barra lateral
+    
+    # Opcional: Cómo se ven los campos al editar
+    fieldsets = (
+        (None, {
+            'fields': ('pregunta', 'respuesta', 'orden')
+        }),
+    )
